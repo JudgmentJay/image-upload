6ea23072f472735bbda0d9a8cc812a22a1185a70
Jay Posten <jdposten@gmail.com>
2021-07-28 16:29:36 -0500

    Initial commit

---
+++ webpack.dev.js
@@ -0,0 +1,38 @@
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common.js')
+
+const cssModuleLoader = {
+	loader: 'css-loader',
+	options: {
+		modules: {
+			localIdentName: '[local]__[hash:base64:5]',
+		}
+	}
+}
+
+module.exports = merge(common, {
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				exclude: /\.module\.scss$/,
+				use: [
+					'style-loader',
+					'css-loader',
+					'sass-loader'
+				],
+				include: /src/
+			},
+			{
+				test: /\.module\.scss$/,
+				use: [
+					'style-loader',
+					cssModuleLoader,
+					'sass-loader'
+				],
+				include: /src/
+			}
+		]
+	},
+	mode: 'development'
+})
